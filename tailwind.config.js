a84e8e4e214c99fa6001193cdfc7bfba25e6687b
MauraYufi <maurayufi@mail.ugm.ac.id>
2024-10-14 12:10:31 +0700

    try swiper

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "hitam": "#000000",
+        "putih": "FFFFFF"
+      }
+    },
+  },
+  plugins: [],
+}
